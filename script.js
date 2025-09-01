3e8e8f41814b757e2f22684a30dcae2f2054b09c
Vishal Wani <93905384+Vishalwani123@users.noreply.github.com>
2025-09-01 20:42:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 function volume_sphere() {
     //Write your code here
-  
+	let rad = parseFloat(document.getElemenetById("radius").value);
+
+	if (isNaN(radius) || radius < 0) {
+        return false; 
+    }
+	const vol = (4 / 3) * Math.PI * Math.pow(radius, 3);
+
+	vol = vol.toFixed(4);
+	
+	document.getElemenetById("volume").value = vol;
+
+	return false;
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
